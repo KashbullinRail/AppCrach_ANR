9618f45b681a7eb72b76558955050c97d402564e
KashbullinRail <kashbullinrail@gmail.com>
2022-11-18 17:10:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/appcrach_anr/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.appcrach_anr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private val btnCrash: Button by lazy { findViewById(R.id.btnCrash) }
+    private val tvSifre: TextView by lazy { findViewById(R.id.tvSifre) }
+    private var timerValue: Int = 20
+    private var thread: Thread? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCrash.setOnClickListener {
+            startTimer()
+        }
+
+    }
+
+    private fun startTimer() {
+        thread = Thread {
+            for (i in 20 downTo 0) {
+                timerValue = i
+                runOnUiThread { updateUI() }
+                Thread.sleep(1000)
+            }
+            runOnUiThread { stopTimer() }
+        }
+        thread?.start()
+    }
+
+    private fun stopTimer() {
+        thread = null
+        timerValue = 20
+        updateUI()
+    }
+
+    private fun updateUI() {
+        tvSifre.text = timerValue.toString()
+        btnCrash.isEnabled = thread == null
+    }
+
+
+}
