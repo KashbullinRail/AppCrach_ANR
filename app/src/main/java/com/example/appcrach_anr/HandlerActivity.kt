08835124e7b4556fc633a211d5e8e770cf20d1d0
KashbullinRail <kashbullinrail@gmail.com>
2022-11-18 17:58:40 +0300

    Handler start Возможность отменить выбор в течении DELAY используя removeCallbacksAndMessages

--- app/src/main/java/com/example/appcrach_anr/HandlerActivity.kt
+++ app/src/main/java/com/example/appcrach_anr/HandlerActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import kotlin.random.Random
 
 class HandlerActivity : AppCompatActivity() {
 
     private val handler = Handler(Looper.getMainLooper())
+    private val token = Any()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +24,21 @@ class HandlerActivity : AppCompatActivity() {
 
     private val universalButtonListener = View.OnClickListener {
         Thread{
-//            when(it.id){
-//                R.id.enableDisableButton ->
-//                    handler.post { toggleTestButtonState }
-//                R.id.DisableButton ->
-//                    handler.post { TestButtonState }
-//                R.id.enableButton ->
-//                    handler.post { toggleButtonState }
-//            }
+            when(it.id){
+                R.id.enableDisableButton ->
+                    handler.post { toggleTestButtonState() }
+                R.id.DisableButton ->
+                    handler.postDelayed({ TestButtonState()}, DELAY)
+                R.id.enableButton ->
+                    handler.postDelayed({toggleButtonState()}, token, DELAY)
+                R.id.btnCancel -> handler.removeCallbacksAndMessages(token)
+            }
         }.start()
+    }
 
-
+    private fun randomColor(){
+        val randomColor = Random.nextInt(255*255*255)
+        btnClick.setBackgroundColor(randomColor)
     }
 
 }
